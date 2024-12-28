2855ea738fc0d9f6f90c29f11ae89d6bb03b26af
andrdrx <yaremiyroman@gmail.com>
2024-12-28 13:41:35 +0200

    2

---
+++ src/1.js
@@ -0,0 +1,264 @@
+// console.log("Hello World!");
+// console.log(234);
+// console.log(323424);
+
+
+// console.log('Something');
+
+// 'String'
+// 'Any string'
+
+// console.log("Another String");
+
+// console.log(`String
+//     Another
+// String`);
+
+
+// console.log("Another String");
+
+// 'String'
+// 'String2'
+
+// console.log('String 3');
+
+// let x = 'String 4';
+// x = 'Another value';
+// let y = 18;
+// console.log(y);
+
+// y = 20;
+// console.log(y);
+
+// x[1];
+
+// console.log(x[1]);
+
+
+
+// let str = 1;
+// str = 2;
+// console.log(str);
+
+
+// str = 100;
+
+// let str = 'any another string';
+// let emptyString = '';
+
+// str.length;
+
+// console.log(str[18]);
+// console.log(str[str.length - 1]);
+
+// 'dfasdfsa dfsdf saf asdf sa'
+
+// 1234521342134
+
+
+// let str = 'Any value';
+// let num = 1432;
+
+
+// console.log(str);
+// console.log(num);
+
+
+// control + /
+// control + d
+
+// alt + shift + DOWN
+
+// let num = 1432;
+// let num2 = 32.5;
+// let num3 = -333;
+// let num3 = -333;
+// let num3 = -333;
+// let num3 = -333;
+// let num3 = -333;
+// let num3 = -333;
+// let num3 = -333;
+// let num3 = -333;
+// let num3 = -333;
+// let num3 = -333;
+// let num3 = -333;
+// let num4 = 4;
+
+
+// let sum = 5 + 9;
+// let sum2 = 5 / 2;
+
+// console.log(sum);
+// console.log(sum2);
+
+
+// let infin = 16 / 0;
+// let minusInfin = -16 / 0;
+
+// let notANumber = 15 / 'String';
+
+
+// 5
+// 10
+// -4
+// Infinity
+// -Infinity
+// NaN // Not A Number
+
+
+// console.log('infin >', infin);
+// condsole.log('-infin >', minusInfin);
+// console.log('-notANumber >', notANumber);
+
+
+
+
+// // Integer
+// 15  // Int
+// 15.5  // Float
+
+
+// 2.4562462346324624e+30
+// 2.4562462346324624 e +30
+// 2.4562462346324624 * 1000000000000000000000000000000
+
+
+
+
+
+
+// String
+// Number
+// Boolean
+
+// let a = true;
+// let b = false;
+
+// let c = 'String any';
+// let g = 2352;
+// let x = true;
+// let y = false;
+
+
+// Null
+// let xx = null;
+// console.log(xx);
+
+// undefined
+// let t = undefined;
+
+
+
+// BigInt
+
+// Symbol
+// const mySymbol = Symbol("annotation");
+// console.log(mySymbol);
+
+
+
+// let anyVar = '234234';  // String
+// let anyNum = 123;   // Number
+// let anyNull = true;  // Boolean
+
+// console.log(typeof anyVar);
+// console.log(typeof anyNum);
+// console.log(typeof anyNull);
+
+
+
+
+// ---> Boolean
+// Boolean(100)
+
+// Boolean(100)
+// true
+// Boolean(-24)
+// true
+// Boolean(0)
+// false
+// Boolean(Infinity)
+// true
+// Boolean(-Infinity)
+// true
+// Boolean(NaN)
+// false
+// Boolean(undefined)
+// false
+// Boolean(null)
+// false
+// Boolean('Hello World');
+// true
+// Boolean('');
+// false
+
+
+// Number(true)
+// 1
+// Number(false)
+// 0
+// Number('Hello World');
+// NaN
+// Number('');
+// 0
+// Number('43');
+// 43
+// Number(null)
+// 0
+// Number(undefined)
+// NaN
+
+// String(ANY VALUE)
+
+
+
+
+// Унарний плюс
+// Конкатенація
+// 2345 + ' Hello ' + null
+// !!  => Boolean()
+
+
+
+
+// +'15' -> 15
+
+
+
+
+// let x = 5;
+
+// x++;
+// x++;
+// x--;
+
+// ++ // інкремент
+// -- // декремент
+
+// let x = 5;
+
+// console.log(x++);
+
+
+
+
+// Логічне І, обʼєднання, and
+// &&
+
+// Логічне АБО, логічне множення, or
+// ||
+
+// Логічне НІ, інверсія
+// !
+// !false = true
+// !true = false
+
+
+
+
+
+'number' + 23 + 32
+
+'number2332'
+
+// тому що конкатенація
---
+++ src/2.js
@@ -0,0 +1,321 @@
+
+// // {
+// //     (true == '')
+// // }
+
+// // {
+// //     /*
+
+// //     console.log(2);
+// //     console.log();
+
+// //     */
+// // }
+
+
+// // // {
+
+// // // }
+
+// // {
+
+// // }
+
+
+// // // let a = 5;
+
+// // // console.log(a);
+
+// // // a = 10;
+// // // console.log(a);
+
+// // // a = 'string';
+// // // console.log(a);
+
+// // // a = false;
+// // // console.log(a);
+
+
+// // // a = null;
+// // // console.log(a);
+
+
+// // // a = undefined;
+// // // console.log(a);
+
+
+
+// // // let a = 5;
+// // // a = 10;
+
+// // // const b = 100;
+// // // b = 15;
+
+// // // console.log(b);
+
+
+
+// // // const x = 101;
+
+// // // console.log(x);
+
+
+// // // let y = 101;
+
+// // // console.log(y);
+
+// // // const x = 10000;
+// // // console.log(x);
+
+
+// // // const a = 10;
+// // // const b = 10;
+// // // const c = 10;
+
+// // // const a = b = c = 10;
+
+
+
+
+// // // let x = 1000; // першопочаткове значення
+
+// // // console.log(x); // 1000
+
+// // // console.log(x); // 1000
+
+// // // x = 99; // значення змінили
+
+// // // console.log(x); // 99
+
+// // // console.log(x); // 99
+
+
+
+// // // let randomNumber = 1000;
+
+// // // console.log(randomNumber);
+
+// // // const message = 'Привіт, я повідомлення';
+
+// // // console.log(message);
+
+// // // const newMessageAnotherWord = 'Нове повідомлення';
+// // // // camelCase
+
+// // // const SOME_CONST = '123';
+
+// // // class NewClass {
+
+// // // }
+
+
+// // // const змінна = 123;
+// // // console.log(змінна);
+
+
+// // const someNumber = 100;
+
+// // const hello1 = 'hello';
+// // const hello2 = 'hello2';
+// // const hello3 = 'hello3';
+// // console.log(hello3);
+// // const hello4 = 'hello4';
+
+// // const mark1 = 'post';
+// // const mark2 = 'post';
+// // const mark3 = 'post';
+// // const mark4 = 'post';
+// // console.log(mark4);
+// // const mark5 = 'post';
+
+// // const new_1 = 24;
+// // const new_2 = 24;
+// // console.log(new_2);
+// // const new_3 = 24;
+// // const new_4 = 24;
+
+// // // down
+// // // DOWN
+
+// // const down = 1;
+// // // console.log(newVariable);
+// // const DOWN = 3;
+
+
+
+// // // const title = 'How to name variables';
+// // // const category = 'JavaScript';
+// // // const formError = 'This field is required';
+
+// // // const car = 'Mazda';
+// // // const food = 'Spagetti';
+// // // const fruit = 'Apple';
+
+
+
+// // // const isFriday = true;
+
+
+
+
+// // // es4
+// // // es6
+
+
+
+
+// // // {
+// // //     const newVariable = 17;
+// // //     console.log(newVariable);
+// // // }
+
+
+// // // console.log(newVariable);
+
+
+// // // hoistingVar
+
+// // // console.log(hoistingVar); // undefined
+
+// // // const hoistingVar = 177; // HOISTING
+// // // console.log(hoistingVar);  // 177
+
+
+
+// // const newVariable = 'Some text';
+// // const anotherVariable = 'Another text';
+
+// // console.log('My log > ', newVariable, anotherVariable, 199);
+
+
+
+
+// if (false) { // condition
+//     // code execution
+// }
+
+
+// // Condition
+
+// const lessZeroMessage = 'Число меньше нуля';
+// const biggerZeroMessage = 'Число більше нуля';
+// const biggerHundred = 'Число більше 100';
+// const lessHundredBiggerZero = 'Число більше 0 але меньше 100';
+// const someNumber = 15;
+
+// if (someNumber > 0) {
+//     if (someNumber > 100) {
+//         console.log(biggerHundred);
+//     } else {
+//         console.log(lessHundredBiggerZero);
+//     }
+// } else {
+//     console.log(lessZeroMessage);
+
+// }
+
+
+
+// // console.log('End of program');
+
+// {}
+
+
+// {
+//     console.log();
+//     {
+//         console.log();
+//         {
+//             console.log();
+//             {
+//                 console.log();
+//             }
+//         }
+//         console.log();
+//     }
+//     console.log();
+
+//     {
+//         {
+
+//         }
+//     }
+
+//     {
+
+//     }
+
+//     {
+
+//     }
+// }
+
+
+
+
+// let testVar = -15;
+
+// if (testVar > 0) {
+//     console.log(testVar);
+// }
+
+
+
+// Number 
+// 12412
+// -2342
+// 0
+// NaN
+// Infinity
+// -Infinity
+
+// f(x) = y
+
+// const newVar = prompt('Введіть будь-що');
+// const message = 'Початок повідомлення: ' + newVar + ' Кінець повідомлення';
+
+// console.log(message);
+// console.log(+newVar);
+
+
+// console.log(typeof +newVar);
+
+// console.log('Output ', +newVar);
+
+
+
+// Запитати у користувача поточну температуру повітря.
+// Якщо вона додатна, то вивести повідомлення що на вулиці тепло з температурою.
+// Якщо відʼємна, то вивести повідомлення, що холодно і скількі градусів.
+
+
+const currentTemperature = +prompt('Скільки градусів?');
+
+if (Number.isNaN(currentTemperature)) {
+    console.log('Невірна температура');
+} else {
+    if (currentTemperature >= 0) {
+        console.log('на вулиці тепло: ', currentTemperature);
+    } else {
+        console.log('на вулиці холодно: ', currentTemperature);
+    }
+}
+
+
+
+// const currentTemperature = +prompt('Скільки градусів?');
+// const message =
+//     currentTemperature >= 0 ? 'на вулиці тепло: ' : 'на вулиці холодно: ';
+
+// console.log(message, currentTemperature);
+
+
+// const N = 10;
+
+// if (N % 3 === 0) {
+
+// }
+// // || && !
+// if (N % 3 === 0 && N % 5 === 0) {
+
+// }
